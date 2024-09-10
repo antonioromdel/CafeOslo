3e519c69695dd287026f751c5fa0e02fd563589f
Antonio Román <150383021+antonioromdel@users.noreply.github.com>
2024-09-10 16:26:56 +0200

    Add files via upload

---
+++ src/products/productos.js
@@ -0,0 +1,28 @@
+export const productos = 
+[
+    {
+        id: 1,
+        nombre:"Pan recién hecho",
+        descripcion: "Caramels liquorice sesame snaps sugar plum I love bonbon. I love I love halvah. I love marzipan cake I love donut tiramisu pudding cotton candy liquorice. Jelly beans chocolate brownie gummi bears dragée jelly-o.",
+        image: "../../public/Icono1.png"
+    },
+    {
+        id: 2,
+        nombre:"Gran gama de pasteles",
+        descripcion:"Caramels liquorice sesame snaps sugar plum I love bonbon. I love I love halvah. I love marzipan cake I love donut tiramisu pudding cotton candy liquorice. Jelly beans chocolate brownie gummi bears dragée jelly-o.",
+        image: "../../public/Icono2.png"
+    },
+    {
+        id: 3,
+        nombre:"Prueba Nuestros Dulces",
+        descripcion:"Caramels liquorice sesame snaps sugar plum I love bonbon. I love I love halvah. I love marzipan cake I love donut tiramisu pudding cotton candy liquorice. Jelly beans chocolate brownie gummi bears dragée jelly-o.",
+        image:"../../public/Icono3.png"
+    },
+    {
+        id: 4,
+        nombre: "Ingredientes Ecológicos",
+        descripcion: "Caramels liquorice sesame snaps sugar plum I love bonbon. I love I love halvah. I love marzipan cake I love donut tiramisu pudding cotton candy liquorice. Jelly beans chocolate brownie gummi bears dragée jelly-o.",
+        image: "../../public/Icono4.png"
+    }
+]
+
